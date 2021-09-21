444c4a9d629daf96bf24066e7de62e79648670c5
Stefan Ruzic <30556980+rule1211@users.noreply.github.com>
2021-09-21 18:56:22 +0200

    ZStack,VStack,Background

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            // Background
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 185/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .ignoresSafeArea(.all)
+            VStack {
+                
+                // Title
+                
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
+                
+                // Credits counter
+                Text("Credits: 1000")
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+            }
+        }
     }
 }
 

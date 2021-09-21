ee36da6bbd52209b8cfd224795fad3d44762296c
Stefan Ruzic <30556980+rule1211@users.noreply.github.com>
2021-09-21 18:07:06 +0200

    Initial Commit

---
+++ Shared/SwiftUISlotsaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISlotsaAppApp.swift
+//  Shared
+//
+//  Created by Stefan Ruzic on 21.9.21..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISlotsaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
